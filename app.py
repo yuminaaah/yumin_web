c1d561eda0fefafaec408afd6621de7eb18169ef
Yumin Kim <yk93@iu.edu>
2023-02-08 00:48:26 -0500

    Added about me Navbar

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template("home.html")
 
+@app.route("/about/")
+def about():
+    return render_template("about.html")
 
 @app.route("/contact/")
 def contact():
