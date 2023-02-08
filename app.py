1dd00226b0b363f7af61b525b6c10fe58a21215e
Yumin Kim <yk93@iu.edu>
2023-02-07 22:08:19 -0500

    Added home page, contact, name

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("home.html")
 
 
-@app.route("/contact")
+@app.route("/contact/")
 def contact():
-    return "<p>Contact Me</p>"
+    return render_template("contact.html")
+
+
+@app.route("/name/")
+@app.route("/name/<username>")
+def name(username="Friend!"):
+    return render_template("name.html", username=username)
